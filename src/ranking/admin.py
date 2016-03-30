cf31eee969f9dade338d575ff7eee4ac013893e8
Marcelo Ferreira <marcelohpf@gmail.com>
2016-03-29 23:27:12 -0300

    Start model to battle

--- src/ranking/admin.py
+++ src/ranking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from ranking import models
+## Register your models here
 
-# Register your models here.
+# Register model for battle
+admin.site.register(models.Battle)
--- src/ranking/models.py
+++ src/ranking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth import models as auth_model
 # Create your models here.
+
+#Battle class with attributes necessary to one participation for one challanger
+class Battle(models.Model):
+    user = models.ForeignKey(auth_model.User)
+    time_begin = models.DateField()
+    time_end = models.DateField()
+    code_winner = models.TextField()
+    
+
