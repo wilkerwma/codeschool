adeb12feed857c80ac6c3107089f1ecb139b7e0b
Marcelo Ferreira <marcelohpf@gmail.com>
2016-03-30 13:25:25 -0300

    Add migration to Battle and method find data

--- src/ranking/urls.py
+++ src/ranking/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'ranking'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^battle/[0-9]+$', views.battle_result, name='result'),
+    url(r'^battle/([0-9]+)$', views.battle_result, name='result'),
 ]
--- src/ranking/views.py
+++ src/ranking/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import Battle
 # Create your views here.
 def index(request):
     return render(request, 'ranking/index.html')
-def battle_result(request):
-    return render(request,'ranking/battle_result.html')
+
+# 'Controller' to view result of a battle
+def battle_result(request,id_battle):
+    print(id_battle)
+    try:
+        result_battle = Battle.objects.get(id=id_battle)
+        return render(request,'ranking/battle_result.html',{"battle":result_battle})
+    except:
+        print("Not found battle")
+        return render(request,'ranking/index.html')
