251c11b7a6ac0c779162e12d07dc0753aa1062c6
Marcelo Ferreira <marcelohpf@gmail.com>
2016-03-29 22:48:35 -0300

    Add basic structure to relatory of battle

--- src/ranking/urls.py
+++ src/ranking/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'ranking'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^battle/[0-9]+$', views.battle_result, name='result'),
 ]
--- src/ranking/views.py
+++ src/ranking/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'ranking/index.html')
+def battle_result(request):
+    return render(request,'ranking/battle_result.html')
