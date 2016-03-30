a590c8ebf53cb9d4b619ea0ccf18a77bfb775a41
Marcelo Ferreira <marcelohpf@gmail.com>
2016-03-30 15:53:05 -0300

    Fix time field without seconds and add data in template

--- src/ranking/views.py
+++ src/ranking/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Battle
 # Create your views here.
 def index(request):
@@ -6,10 +7,15 @@ def index(request):
 
 # 'Controller' to view result of a battle
 def battle_result(request,id_battle):
-    print(id_battle)
+    print( id_battle)
     try:
         result_battle = Battle.objects.get(id=id_battle)
-        return render(request,'ranking/battle_result.html',{"battle":result_battle})
-    except:
-        print("Not found battle")
-        return render(request,'ranking/index.html')
+        response_dict ={"battle": result_battle,
+                        "time_result": result_battle.time_result(),
+                        "len_code": len(result_battle.code_winner) }
+    except Battle.DoesNotExist as e:
+        print("Not found battle"+str(e))
+        raise Http404("Battle not found")
+
+    return render(request,'ranking/battle_result.html',response_dict)
+ 
