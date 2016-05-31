283584547adf80857aabddcfe069c3a98c7350fe
fabiomacedomendes <fabiomacedomendes@gmail.com>
2016-05-31 16:45:22 -0300

    update setup.py with new dependencies

--- setup.py
+++ setup.py
@@ -89,12 +89,11 @@ setup(
         'pygeneric',
         'lazy',
 
-        # These are vendorized until stabilize
+        # These are vendorized until they stabilize
         #'django-viewpack', 
         #'ejudge>=0.3.6',
         #'iospec>=0.2.2',
         #'markio>=0.1.2',
-        
     ],
     extras_require={
         'testing': ['pytest'],
