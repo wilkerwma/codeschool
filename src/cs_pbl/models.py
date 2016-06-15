41e853ed9c89f0779ace11d380d822fa6a4c2cf1
Edson Gomes <edinhogomes@gmail.com>
2016-06-15 15:58:15 -0300

    Criacao do fixture demo.yaml, alteracao na model.py para inclusao de nome para action, criacao de views para Actions e alteracoes no urls.py

--- src/cs_pbl/models.py
+++ src/cs_pbl/models.py
@@ -7,6 +7,7 @@ from codeschool.models import User
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.urlresolvers import reverse
 
 
 @receiver(post_save, sender=Response)
@@ -16,71 +17,85 @@ def my_handle(sender, **kwargs):
 
 
 class HasCategoryMixin:
-	CATEGORY_TRIED = 'tried'
-	CATEGORY_INCOMPLETE = 'incomplete'
-	CATEGORY_CORRECT = 'correct'
-	CATEGORY_CORRECT_AT_FIRST_TRY = 'correct_at_first_try'
-	CATEGORY_CHOICES = [
-		(CATEGORY_TRIED, _('tried')),
-		(CATEGORY_INCOMPLETE, _('incomplete')),
-		(CATEGORY_CORRECT, _('correct')),
-		(CATEGORY_CORRECT_AT_FIRST_TRY, _('correct_at_first_try'))
-	]
-	category = models.CharField(choices=CATEGORY_CHOICES)
+    CATEGORY_TRIED = 'tried'
+    CATEGORY_INCOMPLETE = 'incomplete'
+    CATEGORY_CORRECT = 'correct'
+    CATEGORY_CORRECT_AT_FIRST_TRY = 'correct_at_first_try'
+    CATEGORY_CHOICES = [
+        (CATEGORY_TRIED, _('tried')),
+        (CATEGORY_INCOMPLETE, _('incomplete')),
+        (CATEGORY_CORRECT, _('correct')),
+        (CATEGORY_CORRECT_AT_FIRST_TRY, _('correct_at_first_try'))
+    ]
+    category = models.CharField(choices=CATEGORY_CHOICES)
 
 
 class Action(models.Model):
-	points_tried = models.PositiveIntegerField(default=0)
-	points_incomplete = models.PositiveIntegerField(default=5)
-	points_correct = models.PositiveIntegerField(default=10)
-	points_correct_at_first_try = models.PositiveIntegerField(default=12)
-	activity = models.ForeignKey(Activity)
+    points_tried = models.PositiveIntegerField(default=0)
+    points_incomplete = models.PositiveIntegerField(default=5)
+    points_correct = models.PositiveIntegerField(default=10)
+    points_correct_at_first_try = models.PositiveIntegerField(default=12)
+    activity = models.ForeignKey(Activity)
 
+    name = models.CharField(_('name'), default="Action", max_length=200)
+
+    def get_absolute_url(self):
+        return reverse('detail')
+        # , kwargs={'name':self.name})
+
+    class Meta():
+        verbose_name = _('action')
+        verbose_name_plural = _('actions')
+
+    title = property(lambda x: x.name)
+
+    def __str__(self):
+        return self.name
 
 class Badge(models.Model):
-	name = models.CharField(_('name'), max_length=200)
-	image = models.ImageField(upload_to = 'static/badge', default = '/static/badge/none.jpg')
-	short_description = models.TextField(_('short description'))
-	long_description = models.TextField(_('long description'), blank=True)
+    name = models.CharField(_('name'), max_length=200)
+    image = models.ImageField(upload_to = 'static/badge', default = '/static/badge/none.jpg')
+    short_description = models.TextField(_('short description'))
+    long_description = models.TextField(_('long description'), blank=True)
 
 class GivenBadge(models.TimeStampedModel):
-	badge = models.ForeignKey(Badge)
-	users = models.ForeignKey(models.User)
+    badge = models.ForeignKey(Badge)
+    users = models.ForeignKey(models.User)
 
 class Goal(models.Model):
-	badge = models.ForeignKey(
-		Badge,
-		related_name='goals'
-	)
-	required_points = models.PositiveIntegerField(default=0)
-	required_actions = models.ManyToManyField(Action)
+    badge = models.ForeignKey(
+        Badge,
+        related_name='goals'
+    )
+    required_points = models.PositiveIntegerField(default=0)
+    required_actions = models.ManyToManyField(Action)
 
 class GoalStep(HasCategoryMixin, models.Model):
-	goal = models.ForeignKey(Goal, related_name='steps')
-	action = models.ForeignKey(Action)
-	category = models.CharField(choices=HasCategoryMixin.CATEGORY_CHOICES, null=True, blank=True, max_length=20)
+    goal = models.ForeignKey(Goal, related_name='steps')
+    action = models.ForeignKey(Action)
+    category = models.CharField(choices=HasCategoryMixin.CATEGORY_CHOICES, null=True, blank=True, max_length=20)
 
 class PblUser(models.Model):
-	users = models.OneToOneField(models.User)
-	accumulated_points = models.PositiveIntegerField()
+    users = models.OneToOneField(models.User)
+    accumulated_points = models.PositiveIntegerField()
 
 
 class GivenPoints(HasCategoryMixin, models.TimeStampedModel):
-	action = models.ForeignKey(Action)
-	users = models.ForeignKey(PblUser)
+    action = models.ForeignKey(Action)
+    users = models.ForeignKey(PblUser)
 
-	def value(self):
-		if self.category == self.CATEGORY_CORRECT:
-			return self.action.points_correct
-		elif self.category == self.CATEGORY_INCOMPLETE:
-			return self.action.points_incomplete
-		elif self.category == self.CATEGORY_TRIED:
-			return self.action.points_tried
-		else:
-			raise ValueError('invalid category: %s' % self.category)
+    def value(self):
+        if self.category == self.CATEGORY_CORRECT:
+            return self.action.points_correct
+        elif self.category == self.CATEGORY_INCOMPLETE:
+            return self.action.points_incomplete
+        elif self.category == self.CATEGORY_TRIED:
+            return self.action.points_tried
+        else:
+            raise ValueError('invalid category: %s' % self.category)
 
-	def __int__(self):
-		return self.value()
+    def __int__(self):
+        return self.value()
 
-	def give_points(users):
-		users.accumulated_points = value(self)
+    def give_points(users):
+        users.accumulated_points = value(self)
